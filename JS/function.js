2b4f426c4428a9a5cef56ac3228ec8e353aa8428
Armin <Amesic90@gmail.com>
2024-04-22 16:18:50 +0200

    "..."

--- JS/function.js
+++ JS/function.js
@@ -1,8 +1,24 @@
 function showTable() {
   const table = document.querySelector('#sprogTable');
+  const button = document.querySelector('#showButton');
+
   if (table.style.display === 'none' || table.style.display === '') {
     table.style.display = 'block';
+    button.style.display = 'none';
   } else {
     table.style.display = 'none';
+    button.style.display = 'block';
   }
 }
+// function showTable() {
+//     const table = document.querySelector('#sprogTable');
+//     const button = document.querySelector('#showButton');
+
+//     if (table.style.display === 'none' || table.style.display === '') {
+//       table.style.display = 'block';
+//       button.textContent = 'Hide Programmeringssprog'; // Change button text
+//     } else {
+//       table.style.display = 'none';
+//       button.textContent = 'Vis Programmeringssprog'; // Change button text
+//     }
+//   }

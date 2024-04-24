a09ad5c920247a5b80d35b979ee23b51baf6511d
Armin <Amesic90@gmail.com>
2024-04-24 14:31:38 +0200

    "..."

--- JS/function.js
+++ JS/function.js
@@ -10,29 +10,3 @@ function showTable() {
     button.style.display = 'block';
   }
 }
-
-// function showTable() {
-//     const table = document.querySelector('#sprogTable');
-//     const button = document.querySelector('#showButton');
-
-//     if (table.style.display === 'none' || table.style.display === '') {
-//       table.style.display = 'block';
-//       button.textContent = 'Hide Programmeringssprog'; // Change button text
-//     } else {
-//       table.style.display = 'none';
-//       button.textContent = 'Vis Programmeringssprog'; // Change button text
-//     }
-//   }
-// function showTable() {
-//   const table = document.querySelector('#sprogTable');
-//   const button = document.querySelector('#showButton');
-//   const computedStyle = window.getComputedStyle(table); // Get the computed style
-
-//   if (computedStyle.display === 'none') {
-//     table.style.display = 'block';
-//     button.textContent = 'Skjul Programmeringssprog';
-//   } else {
-//     table.style.display = 'none';
-//     button.textContent = 'Vis Programmeringssprog';
-//   }
-// }
